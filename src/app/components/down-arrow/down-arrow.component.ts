cd238a09aa5f16faad6f865a4f38305950ce1381
Kirk Silva <kirk.ki@hotmail.com>
2024-06-13 01:54:32 -0300

    Adding navigation to down-arrow component.

--- src/app/components/down-arrow/down-arrow.component.ts
+++ src/app/components/down-arrow/down-arrow.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
 
 import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons';
+import { ScrollService } from '../../services/scroll.service';
 
 @Component({
   selector: 'app-down-arrow',
@@ -11,4 +12,37 @@ export class DownArrowComponent {
   @Input() link: string = '';
 
   faAngleDoubleDown = faAngleDoubleDown;
+
+  currentSection: string = '';
+
+  constructor(private scrollService: ScrollService) { }
+
+  scrollToTop() {
+    this.scrollService.scrollToTop();
+    this.currentSection = 'home';
+  }
+
+  scrollToSection(sectionId: string) {
+    this.scrollService.scrollToElement(sectionId);
+    this.currentSection = sectionId;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sections = ['about-me', 'skills', 'projects', 'contact'];
+    let visibleSection = '';
+
+    for (const section of sections) {
+      const element = document.getElementById(section);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        if (rect.top <= 50 && rect.bottom >= 50) {
+          visibleSection = section;
+          break;
+        }
+      }
+    }
+
+    this.currentSection = visibleSection;
+  }
 }
