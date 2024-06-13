0adf7d89875013a12ae11fef7cd9e7247ecf1419
Kirk Silva <kirk.ki@hotmail.com>
2024-06-13 00:47:03 -0300

    Adding scroll service.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { ScrollService } from '../../services/scroll.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  currentSection: string = '';
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private scrollService: ScrollService) { }
 
   scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    this.scrollService.scrollToTop();
+    this.currentSection = 'home';  // Atualize o estado apenas quando clicar em 'Home'
+  }
+
+  scrollToSection(sectionId: string) {
+    this.scrollService.scrollToElement(sectionId);
+    this.currentSection = sectionId;  // Atualize o estado ao navegar para outras seções
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sections = ['home', 'about-me', 'skills', 'projects', 'contact'];
+    let visibleSection = '';
+
+    for (const section of sections) {
+      const element = document.getElementById(section);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        if (rect.top <= 50 && rect.bottom >= 50) {  // 50 é um valor de margem superior para ajustar a ativação
+          visibleSection = section;
+          break;
+        }
+      }
+    }
+
+    this.currentSection = visibleSection;
   }
 }
---
+++ src/app/services/scroll.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScrollService {
+
+  scrollToElement(elementId: string): void {
+    const element = document.getElementById(elementId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
