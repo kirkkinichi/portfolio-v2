5d308a0e3f5971df867ebc5b937570eadbc54e5a
Kirk Silva <kirk.ki@hotmail.com>
2024-06-12 23:42:59 -0300

    Adding navigation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'portfolio';
+export class AppComponent implements OnInit {
+
+	title = 'portfolio'
+
+	constructor(private router: Router, private route: ActivatedRoute) { }
+
+	ngOnInit() {
+		this.route.fragment.subscribe(fragment => {
+			if (fragment) {
+				this.scrollToSection(fragment);
+			}
+		});
+	}
+
+	scrollToSection(sectionId: string) {
+		const element = document.getElementById(sectionId);
+		if (element) {
+			element.scrollIntoView({ behavior: 'smooth' });
+		}
+	}
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
