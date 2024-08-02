a0ce792076cc67d10ba21effb5886fec89976af0
Kirk Silva <kirk.ki@hotmail.com>
2024-08-02 20:20:21 -0300

    Removing Live Preview button.

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -7,7 +7,6 @@ interface Project {
 	tools: string;
 	imageUrl: string;
 	githubUrl: string;
-	liveUrl: string;
 	category: string;
 }
 
@@ -26,7 +25,6 @@ export class ProjectsComponent {
 			description: 'Kirk and Morty is a single-page application that uses Angular framework and The Rick and Morty API to list and detail the Characters, Episodes, and Locations featured in the series.',
 			imageUrl: 'assets/img/kirk-and-morty-project.png',
 			githubUrl: 'https://github.com/kirkkinichi/rick-morty-project',
-			liveUrl: 'https://kirk-and-morty.kirksilva.com/',
 			category: 'Angular, Bootstrap, Docker, Font Awesome, HTML, Node.js, SCSS, TypeScript'
 		},
 		{
@@ -35,7 +33,6 @@ export class ProjectsComponent {
 			description: 'Minesweeper Game Project, developed in Java during the course: Java 2022 COMPLETO: Do Zero ao Profissional + Projetos! - Instructed by: Leonardo Moura Leitao and Cod3r Cursos Online, through the Udemy platform.',
 			imageUrl: 'assets/img/minesweeper-java-project.png',
 			githubUrl: 'https://github.com/kirkkinichi/minesweeper-java',
-			liveUrl: '',
 			category: 'Java'
 		},
 		{
@@ -44,7 +41,6 @@ export class ProjectsComponent {
 			description: '',
 			imageUrl: 'assets/img/portfolio-project.png',
 			githubUrl: 'https://github.com/kirkkinichi/portfolio-v2',
-			liveUrl: '',
 			category: 'Angular'
 		}
 	];
